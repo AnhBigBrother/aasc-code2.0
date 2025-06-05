f7eaecc562735af7cf5df690eea921efaa4ccaeb
anh.bigbro <anh.bigbrother@gmail.com>
2025-06-05 13:09:23 +0700

    update build.sh script

--- board-games/build.sh
+++ board-games/build.sh
@@ -1,6 +1,10 @@
-cd client && npm run build && cd ..
+cd client
+npm install
+npm run build
+cd ..
 mkdir server/static -p
 cp -r client/dist/* server/static
 cd server
+npm install
 npm run build
 node dist/main.js
